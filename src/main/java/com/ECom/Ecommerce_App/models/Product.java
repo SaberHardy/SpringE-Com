8fe3ffa4b6773b20b9b5955d8ccb52ca6737e248
SaberHardy <samirsamado5@gmail.com>
2024-07-30 21:46:05 -0700

    create Product model

--- src/main/java/com/ECom/Ecommerce_App/models/Product.java
+++ src/main/java/com/ECom/Ecommerce_App/models/Product.java
@@ -1,4 +1,16 @@
 package com.ECom.Ecommerce_App.models;
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+// create lombok to generate getters and setters
+@Data
+@AllArgsConstructor
+//@Component
 public class Product {
+
+    private int productId;
+    private String productName;
+    private double price;
 }
