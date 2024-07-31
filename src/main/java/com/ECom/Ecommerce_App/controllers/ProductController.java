4b92f8edb699bed78299614ce12c8b7f165e81f7
SaberHardy <samirsamado5@gmail.com>
2024-07-30 21:47:24 -0700

    create ProductController to redirect to list products

---
+++ src/main/java/com/ECom/Ecommerce_App/controllers/ProductController.java
@@ -0,0 +1,20 @@
+package com.ECom.Ecommerce_App.controllers;
+
+import com.ECom.Ecommerce_App.models.Product;
+import com.ECom.Ecommerce_App.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @RequestMapping("/products")
+    public List<Product> getProducts() {
+       return productService.getProducts();
+    }
+}
