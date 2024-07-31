f2f71f4d49d92e4692d56e7de3c95f37dcdaebf3
SaberHardy <samirsamado5@gmail.com>
2024-07-30 21:46:42 -0700

    create ProductService to retrun list products

--- src/main/java/com/ECom/Ecommerce_App/services/ProductService.java
+++ src/main/java/com/ECom/Ecommerce_App/services/ProductService.java
@@ -1,5 +1,22 @@
 package com.ECom.Ecommerce_App.services;
 
+import com.ECom.Ecommerce_App.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class ProductService {
 
+    List<Product> products = Arrays.asList(
+            new Product(1, "Apple", 10.0),
+            new Product(2, "Banana", 20.0),
+            new Product(3, "Orange", 30.0)
+    );
+
+    public List<Product> getProducts() {
+        return products;
+    }
 }
