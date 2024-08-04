d50e951e8a376b33a09d21265554df1d43ba3f9f
SaberHardy <samirsamado5@gmail.com>
2024-08-03 22:52:51 -0700

    add default data into productsDB

--- src/main/java/com/ECom/Ecommerce_App/models/Product.java
+++ src/main/java/com/ECom/Ecommerce_App/models/Product.java
@@ -1,6 +1,8 @@
 package com.ECom.Ecommerce_App.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.AllArgsConstructor;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 //@Component
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int productId;
     private String productName;
     private double price;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into product (product_name, price)
+values
+('Apple', 10.0),
+('Banana', 20.0),
+('Orange', 30.0),
+('Mango', 40.0);
